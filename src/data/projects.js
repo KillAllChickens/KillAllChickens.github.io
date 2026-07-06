47f62f30b449034a06dbc01ef342babddcfd65a9
KillAllChickens <vance@killallchickens.org>
2026-07-06 00:35:31 -0500

    ADD: FILE / INITIAL COMMIT

---
+++ src/data/projects.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Argus Panoptes",
+    category: "CLI",
+    tags: ["Concurrency"],
+    stack: ["Golang"],
+    description: "An OSINT tool designed to discover public accounts of any indidual across 170+ different social medias from just a username, all is 5 seconds or less.",
+    link: "https://github.com/KillAllChickens/argus",
+  },
+  {
+    id: 2,
+    title: "Burrow",
+    category: "Networking",
+    tags: ["WebRTC", "Websocket"],
+    stack: ["Golang"],
+    description: "Send files to anyone via p2p webRTC, zero size limits, and oversea tests show 7MB+/s consistantly. Accompanied by a no-log custom websocket signalling server.",
+    link: "https://github.com/KillAllChickens/burrow",
+  },
+  {
+    id: 3,
+    title: "Diary CLI",
+    category: "CLI",
+    tags: ["Encryption", "TUI"],
+    stack: ["Golang"],
+    description: "Write daily diary entries from your terminal, dead simple, uses neovim for editting, has AES-256 encryption for all entries and markdown support.",
+    link: "https://github.com/KillAllChickens/diary-cli",
+  },
+  {
+    id: 4,
+    title: "SuperStudies",
+    category: "Web",
+    tags: ["Web"],
+    description: "A real-time messaging platform that was once deployed in my school, bypassed browser restrictions. Features WebRTC video chat, an AI note-taker, and more.",
+    link: "",
+  }
+]
